68102f2796bb94fd4062d6d4645048b4d171b39f
Bohdan Zlotko <bohdanzlotko@gmail.com>
2023-08-21 00:18:09 +0300

    add

---
+++ models/contacts.js
@@ -0,0 +1,39 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPats = path.join(__dirname, "contacts.json");
+
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPats);
+  return JSON.parse(contacts);
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
+
+const addContact = async (body) => {};
+
+const updateContact = async (contactId, body) => {};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
